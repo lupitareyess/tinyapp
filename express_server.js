afb326ad634ee5096ce180dae147ef0e05f02540
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-25 01:41:28 -0500

    Eslint

--- express_server.js
+++ express_server.js
@@ -82,7 +82,7 @@ app.get("/urls/:id", (req, res) => {
     const templateVars = {
       user: null,
       error: "Oops! Page does not exist."
-    }
+    };
     return res.status(404).render("errors", templateVars);
   }
 
@@ -90,7 +90,7 @@ app.get("/urls/:id", (req, res) => {
     const templateVars = {
       user: null,
       error: "You do not have access to this URL."
-    }
+    };
     return res.status(403).render("errors", templateVars);
   }
 
@@ -113,7 +113,7 @@ app.get("/u/:id", (req, res) => {
     const templateVars = {
       user: null,
       error: "Oops! Short URL does not exist."
-    }
+    };
     return res.status(400).render("errors", templateVars);
   }
 
@@ -173,14 +173,14 @@ app.post("/register", (req, res) => {
     const templateVars = {
       user: null,
       error: "Please enter a valid email address and password."
-    }
+    };
     return res.status(400).render("errors", templateVars);
   }
   if (getUserByEmail(email, users)) {
     const templateVars = {
       user: null,
       error: "Email associated with existing account."
-    }
+    };
     return res.status(400).render("errors", templateVars);
   }
   users[userID] = {
@@ -201,7 +201,7 @@ app.post("/login", (req, res) => {
     const templateVars = {
       user: null,
       error: "Please enter a valid email address and password."
-    }
+    };
     return res.status(400).render("errors", templateVars);
   }
 
@@ -211,7 +211,7 @@ app.post("/login", (req, res) => {
     const templateVars = {
       user: null,
       error: "User not found. Please register to get started."
-    }
+    };
     return res.status(403).render("errors", templateVars);
   }
 
@@ -219,7 +219,7 @@ app.post("/login", (req, res) => {
     const templateVars = {
       user: null,
       error: "Incorrect email or password. Please try again."
-    }
+    };
     return res.status(403).render("errors", templateVars);
   }
 

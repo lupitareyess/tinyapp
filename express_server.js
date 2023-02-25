c9b9e7d57692687bc873d51899ac917b428429d4
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-24 22:45:24 -0500

    Organize code

--- express_server.js
+++ express_server.js
@@ -1,88 +1,66 @@
+// -------------- DEPENDENCIES & PORT ----------------- //
 const express = require("express");
-const app = express();
-const PORT = 8080;
-const cookieParser = require('cookie-parser');
 const bcrypt = require("bcryptjs");
+const methodOverride = require("method-override");
 const cookieSession = require("cookie-session");
 const { generateRandomString, getUserByEmail, urlsForUser } = require("./helpers");
-const methodOverride = require("method-override");
 
+const app = express();
+const PORT = 8080;
+
+// ---------------- MIDDLEWARE ------------------ //
 app.set("view engine", "ejs");
 
-// parses the body
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(methodOverride("_method"));
 app.use(cookieSession({
   name: "session",
   keys: ["key1"]
 }));
-app.use(methodOverride("_method"));
 
-
+// ----------------- DATA --------------------- //
 const { urlDatabase, users } = require("./data");
 
+// -------------- GET REQUESTS ---------------- //
 
-//Change this later, will be for homepage
+// Landing page, redirects to login page if user is not logged in
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const userID = req.session.user_id;
+  if (!userID) {
+    return res.redirect("/login");
+  }
+
+  return res.redirect("/urls");
 });
 
-// Pass URL data to urls_index template
+// Display users URLs if logged in
 app.get("/urls", (req, res) => {
   const userID = req.session.user_id;
+  const user = users[userID];
+
+  if (!user) {
+    return res.send("Please log in or register to access URLs.")
+  }
   const templateVars = {
     urls: urlsForUser(userID, urlDatabase),
     user: users[userID]
   }
-  console.log(userID);
-  console.log(templateVars);
 
-  if (!templateVars.user) {
-    return res.send("Please log in or register to access URLs.")
-  }
   return res.render("urls_index", templateVars);
 });
 
-// Render form on web page to present to user 
+// Page to create new URL, user must be logged in
 app.get("/urls/new", (req, res) => {
   const userID = req.session.user_id;
+  if (!userID) {
+    return res.redirect("/login")
+  }
   const templateVars = {
     user: users[userID]
   }
-  if (!templateVars.user) {
-    return res.redirect("/login")
-  }
+
   return res.render("urls_new", templateVars);
 });
-
-// Handle the form submission
-app.post("/urls", (req, res) => {
-  const userID = req.session.user_id;
-
-  if (!userID) {
-    return res.send("Please login to shorten a URL")
-  }
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = {
-    longURL: req.body.longURL,
-    userID: req.session.user_id
-  }
-  return res.redirect(`/urls/${shortURL}`);
-})
-
-// Redirect to the appropriate long URL
-app.get("/u/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id]
-
-  if (longURL === undefined) {
-    return res.status(400).send("Short URL does not exist")
-  }
-  //make sure its not being added to database
-  console.log(urlDatabase[id].longURL);
-  res.redirect(urlDatabase[id].longURL);
-});
-
 
 // Page that displays a single URL and its shortened form
 app.get("/urls/:id", (req, res) => {
@@ -105,14 +83,127 @@ app.get("/urls/:id", (req, res) => {
     return res.status(403).send("You do not have access")
   }
 
-
   const templateVars = {
     id: id,
     longURL: urlDatabase[id].longURL,
     user: users[userID]
   }
+
   res.render("urls_show", templateVars);
 });
+
+// Redirection from shortURL to the appropriate longURL
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id]
+
+  if (longURL === undefined) {
+    return res.status(400).send("Short URL does not exist")
+  }
+
+  return res.redirect(urlDatabase[id].longURL);
+});
+
+// Eender registration template 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.session.user_id]
+  };
+  if (templateVars.user) {
+    return res.redirect("/urls");
+  }
+
+  return res.render("urls_register", templateVars);
+});
+
+// Renders login template
+app.get("/login", (req, res) => {
+  const templateVars = {
+    user: users[req.session.userID]
+  }
+  if (templateVars.user) {
+    return res.redirect("/urls")
+  }
+
+  return res.render("urls_login", templateVars)
+})
+
+// ------------ POST REQUESTS --------------- //
+
+// Handle the form submission, creates shortURL
+app.post("/urls", (req, res) => {
+  const userID = req.session.user_id;
+
+  if (!userID) {
+    return res.send("Please login to shorten a URL")
+  }
+
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id
+  };
+
+  return res.redirect(`/urls/${shortURL}`);
+});
+
+// Registration endpoint that handles the registration data
+app.post("/register", (req, res) => {
+  const userID = generateRandomString()
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  if (email === "" || password === "") {
+    return res.status(400).send('400. Please enter a valid email address and password');
+  }
+  else if (getUserByEmail(email, users)) {
+    return res.status(404).send('400. Email associated with existing account')
+  }
+  else {
+    users[userID] = {
+      userID,
+      email,
+      password: hashedPassword
+    };
+
+    res.session.user_id = userID;
+    return res.redirect("/urls");
+  }
+});
+
+// Login Endpoint
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email === '' || password === '') {
+    return res.status(400).send('400. Email and password is required.');
+  }
+
+  const user = getUserByEmail(email, users)
+
+  if (!user) {
+    return res.status(404).send("404. User not found.")
+  }
+
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(403).send("Incorrect email or password. Please try again.")
+  }
+
+  req.session.user_id = user.userID;
+  return res.redirect("/urls");
+
+});
+
+// Logout endpoint, clear cookies
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/login");
+});
+
+
+// ------------ PUT & DELETE REQUESTS ------------- //
 
 // Delete a URL
 app.delete("/urls/:id/delete", (req, res) => {
@@ -168,90 +259,7 @@ app.put("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-// Login route - render login template
-app.get("/login", (req, res) => {
-  const templateVars = {
-    user: users[req.session.userID]
-  }
-  if (templateVars.user) {
-    return res.redirect("/urls")
-  }
-  return res.render("urls_login", templateVars)
-})
-
-// Login Endpoint
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  if (email === '' || password === '') {
-    return res.status(400).send('400. Email and password is required.');
-  }
-
-  const user = getUserByEmail(email, users)
-  console.log("user", user)
-  if (!user) {
-    return res.status(404).send("404. User not found.")
-  }
-  console.log("user password", user.password);
-  console.log("hashed", hashedPassword);
-  if (!bcrypt.compareSync(password, user.password)) {
-    return res.status(403).send("Incorrect password")
-  }
-
-  req.session.user_id = user.userID;
-  return res.redirect("/urls");
-
-});
-
-// Logout endpoint - clear cookies
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/login");
-});
-
-// Registration route - render registration template
-app.get("/register", (req, res) => {
-  const templateVars = {
-    user: users[req.session.user_id]
-  };
-  if (templateVars.user) {
-    return res.redirect("/urls");
-  }
-  return res.render("urls_register", templateVars);
-});
-
-// Registration endpoint that handles the registration data:
-app.post("/register", (req, res) => {
-  const userID = generateRandomString()
-  const email = req.body.email;
-  const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  if (email === "" || password === "") {
-    return res.status(404).send('400. Request has resulted in an error');
-  }
-  else if (getUserByEmail(email, users)) {
-    return res.status(404).send('400. Email associated with existing account')
-  }
-  else {
-    users[userID] = {
-      userID,
-      email,
-      password: hashedPassword
-    };
-
-    res.session.user_id = userID;
-    return res.redirect("/urls");
-  }
-});
-
-
-
-
-
-// listen should always be at the end 
+// ---------- LISTENING PORT ------------ //
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
